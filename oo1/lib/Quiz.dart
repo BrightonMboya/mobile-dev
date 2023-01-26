50fb4166a1abd6509e7b215c6efe87b1bbf68abb
tony <b.mboya@alustudent.com>
2023-01-26 13:26:18 +0200

    learned some flutter concepts

---
+++ oo1/lib/Quiz.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+import './answer.dart';
+import './question.dart';
+
+class Quiz extends StatelessWidget {
+  final Function answerQuestion;
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+
+  Quiz(this.answerQuestion, this.questions, this.questionIndex);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(questions[questionIndex]['questionText']),
+        ...(questions[questionIndex]["answers"] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion(answer['score']), answer['text']);
+        }).toList()
+      ],
+    );
+  }
+}
--- oo1/lib/main.dart
+++ oo1/lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import './Quiz.dart';
+import "./result.dart";
 
 void main() {
   runApp(MyApp());
 }
 
 const questions = [
-  "What is Your Name ",
-  "Who is Your Lover",
-  "Do you want to get Married"
+  {
+    "questionText": "What is Your Name ",
+    "answers": [
+      {"text": "Tonero", "score": 40},
+      {"text": "Lawrence", "score": 30},
+      {"text": "Brighton", "score": 30}
+    ]
+  },
+  {
+    "questionText": "Who is Your Lover",
+    "answers": [
+      {"text": "TS", "score": 40},
+      {"text": "Piano", "score": 20},
+      {"text": "Electric Guitar", "score": 40}
+    ]
+  },
+  {
+    "questionText": "Do you want to get Married",
+    "answers": [
+      {"text": "Daah", "score": 70},
+      {"text": "Meeh", "score": 20},
+      {"text": "Ir izz warr ir izzz", "score": 10}
+    ]
+  },
 ];
+
+var totalScore = 0;
 
 class MyApp extends StatefulWidget {
   @override
@@ -20,11 +45,14 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var questionIndex = 0;
-  void answerQuestion() {
+  void answerQuestion(int score) {
+    totalScore += score;
     setState(() {
       questionIndex += 1;
     });
-    print(questionIndex);
+    if (questionIndex < questions.length) {
+      print("we have more questions");
+    }
   }
 
   @override
@@ -34,19 +62,9 @@ class MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text("Fuckin Annoying"),
           ),
-          body: Column(
-            children: [
-              Text(questions[questionIndex]),
-              ElevatedButton(
-                onPressed: answerQuestion,
-                child: Text("Answer 1"),
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text("Answer 2"),
-              ),
-            ],
-          )),
+          body: questionIndex < questions.length
+              ? Quiz(answerQuestion, questions, questionIndex)
+              : Result(totalScore)),
     );
   }
 }
